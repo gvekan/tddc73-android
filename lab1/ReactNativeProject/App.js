bcad3dadb8acc9acd56e86258090323b3eb13fe4
Gustav <gusan092@student.liu.se>
2019-11-11 21:04:59 +0100

    Work on react native

--- lab1/ReactNativeProject/App.js
+++ lab1/ReactNativeProject/App.js
@@ -14,6 +14,10 @@ import {
   View,
   Text,
   StatusBar,
+  Button,
+  ToolbarAndroid,
+  TextInput,
+  Image
 } from 'react-native';
 
 import {
@@ -25,8 +29,73 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 
 const App: () => React$Node = () => {
+  const ButtonRow = () => (
+      <View
+        style={{
+          marginTop: 16,
+          flexDirection: 'row',
+          justifyContent: 'space-evenly'
+        }}>
+          <Button title='Button'/>
+          <Button title='Button'/>
+      </View>
+    );
+
   return (
     <>
+      <StatusBar backgroundColor="#1a574b" barStyle="dark-content" />
+      <View
+        style={{
+          padding: 16,
+          backgroundColor:"#2d8577",
+        }}>
+          <Text
+            style={{
+              fontSize: 30,
+              color: 'white'
+            }}          
+          >
+            Example 1
+          </Text>
+        </View>
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'column',
+          margin: 16
+        }}>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-around"
+            }}
+          >
+            <Image style={{width:150, height:150}} source={require('./images/lab1_image_small.png')}/>
+          </View>
+          <ButtonRow/>
+          <ButtonRow/>
+          <View
+            style={{
+              flexDirection: 'row',
+              marginTop: 16
+            }}>
+            <Text>
+              Email
+            </Text>
+            <TextInput 
+              style={{
+                width: 200,
+                marginStart: 32,
+                marginEnd: 32,
+                borderBottomColor: "#ce2366",
+                borderBottomWidth: 1,
+                padding: 0
+              }}/>
+          </View>
+        </View>
+      </>
+    );
+    /*<>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         <ScrollView
@@ -69,7 +138,7 @@ const App: () => React$Node = () => {
         </ScrollView>
       </SafeAreaView>
     </>
-  );
+  );*/
 };
 
 const styles = StyleSheet.create({
